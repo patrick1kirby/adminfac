e0e87cca2dd0e3b82327e7cc03b9a5ec96be01f5
Patrick Kirby <pkirby@Patrick-Kirbys-MacBook-Pro.local>
2011-03-21 23:06:45 -0700

    removed

--- config/initializers/admin_data.rb
+++
@@ -1,7 +0,0 @@
-require "admin_data"
-
-AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
-  config.is_allowed_to_update = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
-end
-
