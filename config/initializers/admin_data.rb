b3a1a5cb0626390be430003eb61864617fb7611a
Patrick Kirby <pkirby@Patrick-Kirbys-MacBook-Pro.local>
2011-03-21 22:54:35 -0700

    update

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,3 +1,5 @@
+require "admin_data"
+
 AdminData.config do |config|
   config.is_allowed_to_view = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
   config.is_allowed_to_update = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
