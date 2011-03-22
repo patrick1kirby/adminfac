5c3e1204386223b50ed18f82cacd26bebb825999
Patrick Kirby <pkirby@Patrick-Kirbys-MacBook-Pro.local>
2011-03-21 23:20:29 -0700

    removed

--- vendor/plugins/admin_data/config/admin_data.rb
+++
@@ -1,4 +0,0 @@
-AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
-  config.is_allowed_to_update = lambda {|controller| return true if (Rails.env.development? || Rails.env.staging?) }
-end
