1b33b613b96d945f606294554c0db067ad1f379f
Patrick Kirby <pkirby@Patrick-Kirbys-MacBook-Pro.local>
2011-03-28 19:37:38 -0700

    add uuid to csv

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,9 +7,10 @@ class Location < ActiveRecord::Base
     @locations = Location.find_by_sql("SELECT * FROM `locations` WHERE (" + query + ") ORDER BY locations.id desc LIMIT 10000");
 
     return csv_data = FasterCSV.generate do |csv|
-      csv << [:name, :address, :locality, :region, :postcode, :tel, :fax, :category, :website, :latitude, :longitude]
+      csv << [:uuid, :name, :address, :locality, :region, :postcode, :tel, :fax, :category, :website, :latitude, :longitude]
       @locations.each do |location|
         csv << [
+        location.uuid,
         location.name,
         location.address,
         location.locality,
